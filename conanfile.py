b9e8119ccb2644623d64ccee22c0d65d212b4ae1
Javier G. Sogo <jgsogo@gmail.com>
2018-01-25 07:46:26 +0100

    add all gtest libraries

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,14 @@
 
 from conans import ConanFile, CMake, tools
 import os
+import shutil
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    url = "https://github.com/bincrafters/conan-libname"
-    description = "Keep it short"
+class VariantConan(ConanFile):
+    name = "variant"
+    version = "1.3.0"
+    url = "https://github.com/bincrafters/conan-variant"
+    description = "C++17 `std::variant` for C++11/14/17 https://mpark.github.io/variant"
 
     # Indicates License type of the packaged library
     license = "MIT"
@@ -23,8 +24,8 @@ class LibnameConan(ConanFile):
 
     # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"build_tests": [True, False]}
+    default_options = "build_tests=True"
 
     # Custom attributes for Bincrafters recipe conventions
     source_subfolder = "source_subfolder"
@@ -32,14 +33,25 @@ class LibnameConan(ConanFile):
 
     # Use version ranges for dependencies unless there's a reason not to
     requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/[>=1.2.11]@conan/stable"
+        "gtest/[>=1.8.0]@jgsogo/testing",
     )
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
+        source_url = "https://github.com/mpark/variant"
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
         extracted_dir = self.name + "-" + self.version
+
+        # Work to remove 'deps' directory (conan will handle them)
+        shutil.rmtree(os.path.join(extracted_dir, "3rdparty"))
+        tools.replace_in_file(os.path.join(extracted_dir, "test", "CMakeLists.txt"),
+'''
+  add_subdirectory(${CMAKE_SOURCE_DIR}/3rdparty/googletest/googletest
+                   ${CMAKE_BINARY_DIR}/3rdparty/googletest/googletest)
+''',
+'''
+  include(${CONAN_GTEST_ROOT}/cmake/internal_utils.cmake)
+''')
+        tools.replace_in_file(os.path.join(extracted_dir, "test", "CMakeLists.txt"), 'gtest_main', '${CONAN_LIBS_GTEST}')
 
         #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
@@ -47,7 +59,8 @@ class LibnameConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
+        if self.options.build_tests:
+            cmake.definitions["MPARK_VARIANT_INCLUDE_TESTS"] = "mpark"  # Adding 'libc++' will trigger an ExternalProjectAdd for llvm
         cmake.configure(build_folder=self.build_subfolder)
         cmake.build()
         cmake.install()
