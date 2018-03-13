128e00bf761db316ec50fc087cfcfcab7b52340e
Javier G. Sogo <jgsogo@gmail.com>
2018-03-13 12:39:41 +0100

    modernize ci jobs

--- .travis/install.sh
+++ .travis/install.sh
@@ -8,6 +8,7 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew outdated pyenv || brew upgrade pyenv
     brew install pyenv-virtualenv
     brew install cmake || true
+    brew install nasm || true
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
@@ -17,10 +18,9 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv virtualenv 2.7.10 conan
     pyenv rehash
     pyenv activate conan
+else
+	sudo apt-get install -y nasm autoconf dh-autoreconf
 fi
 
-pip install conan --upgrade
-pip install conan_package_tools bincrafters_package_tools
-
+pip install conan_package_tools  bincrafters_package_tools
 conan user
-conan remote list
